9ccbded84a99dce214ed0b0329f169d445614df5
nursatj <nursat@mpower-social.com>
2018-01-08 17:19:31 +0600

    resolved error

--- opensrp-etl/src/main/java/org/opensrp/etl/entity/MIS1ReportEntity.java
+++ opensrp-etl/src/main/java/org/opensrp/etl/entity/MIS1ReportEntity.java
@@ -17,136 +17,137 @@ import org.opensrp.etl.report.NutritionReport;
 @Entity
 @Table(name = "mis1report")
 public class MIS1ReportEntity {
-	@Id
-	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "mis1report_id_seq")
-	@SequenceGenerator(name = "mis1report_id_seq", sequenceName = "mis1report_id_seq", allocationSize = 1)
-	private int id;
-	
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "mis1report_id_seq")
+    @SequenceGenerator(name = "mis1report_id_seq", sequenceName = "mis1report_id_seq", allocationSize = 1)
+    private int id;
+
     private String division;
     private String district;
     private String upazilla;
     private String ward;
     private String geo_union;
-    private String unit;    
+    private String unit;
     private String reporting_year;
     private String reporting_month;
-    
+
     @Embedded
-	private FamilyPlanningReport familyPlanningReport;
-    //private MaternityCareReport maternityCareReport;
-    //private ChildCareReport childCareReport;
-    //private BirthAndDeathReport birthAndDeathReport;
-    //private NutritionReport nutritionReport;
+    private FamilyPlanningReport familyPlanningReport;
+    @Embedded
+    private MaternityCareReport maternityCareReport;
+    @Embedded
+    private ChildCareReport childCareReport;
+    @Embedded
+    private BirthAndDeathReport birthAndDeathReport;
+    @Embedded
+    private NutritionReport nutritionReport;
 
-	public MIS1ReportEntity() {
-		// TODO Auto-generated constructor stub
-	}
+    public MIS1ReportEntity() {
+    }
 
-	public FamilyPlanningReport getFamilyPlanningReport() {
-		return familyPlanningReport;
-	}
+    public FamilyPlanningReport getFamilyPlanningReport() {
+        return familyPlanningReport;
+    }
 
-	public void setFamilyPlanningReport(FamilyPlanningReport familyPlanningReport) {
-		this.familyPlanningReport = familyPlanningReport;
-	}
-		
+    public void setFamilyPlanningReport(
+            FamilyPlanningReport familyPlanningReport) {
+        this.familyPlanningReport = familyPlanningReport;
+    }
 
-/*	public MaternityCareReport getMaternityCareReport() {
-		return maternityCareReport;
-	}
+    public MaternityCareReport getMaternityCareReport() {
+        return maternityCareReport;
+    }
 
-	public void setMaternityCareReport(MaternityCareReport maternityCareReport) {
-		this.maternityCareReport = maternityCareReport;
-	}
+    public void setMaternityCareReport(MaternityCareReport maternityCareReport) {
+        this.maternityCareReport = maternityCareReport;
+    }
 
-	public BirthAndDeathReport getBirthAndDeathReport() {
-		return birthAndDeathReport;
-	}
+    public BirthAndDeathReport getBirthAndDeathReport() {
+        return birthAndDeathReport;
+    }
 
-	public void setBirthAndDeathReport(BirthAndDeathReport birthAndDeathReport) {
-		this.birthAndDeathReport = birthAndDeathReport;
-	}
+    public void setBirthAndDeathReport(BirthAndDeathReport birthAndDeathReport) {
+        this.birthAndDeathReport = birthAndDeathReport;
+    }
 
-	public ChildCareReport getChildCareReport() {
-		return childCareReport;
-	}
+    public ChildCareReport getChildCareReport() {
+        return childCareReport;
+    }
 
-	public void setChildCareReport(ChildCareReport childCareReport) {
-		this.childCareReport = childCareReport;
-	}
+    public void setChildCareReport(ChildCareReport childCareReport) {
+        this.childCareReport = childCareReport;
+    }
 
-	public NutritionReport getNutritionReport() {
-		return nutritionReport;
-	}
+    public NutritionReport getNutritionReport() {
+        return nutritionReport;
+    }
 
-	public void setNutritionReport(NutritionReport nutritionReport) {
-		this.nutritionReport = nutritionReport;
-	}
-*/
-	public String getDivision() {
-		return division;
-	}
+    public void setNutritionReport(NutritionReport nutritionReport) {
+        this.nutritionReport = nutritionReport;
+    }
 
-	public void setDivision(String division) {
-		this.division = division;
-	}
+    public String getDivision() {
+        return division;
+    }
 
-	public String getDistrict() {
-		return district;
-	}
+    public void setDivision(String division) {
+        this.division = division;
+    }
 
-	public void setDistrict(String district) {
-		this.district = district;
-	}
+    public String getDistrict() {
+        return district;
+    }
 
-	public String getUpazilla() {
-		return upazilla;
-	}
+    public void setDistrict(String district) {
+        this.district = district;
+    }
 
-	public void setUpazilla(String upazilla) {
-		this.upazilla = upazilla;
-	}
+    public String getUpazilla() {
+        return upazilla;
+    }
 
-	public String getWard() {
-		return ward;
-	}
+    public void setUpazilla(String upazilla) {
+        this.upazilla = upazilla;
+    }
 
-	public void setWard(String ward) {
-		this.ward = ward;
-	}
+    public String getWard() {
+        return ward;
+    }
 
-	public String getGeo_union() {
-		return geo_union;
-	}
+    public void setWard(String ward) {
+        this.ward = ward;
+    }
 
-	public void setGeo_union(String geo_union) {
-		this.geo_union = geo_union;
-	}
+    public String getGeo_union() {
+        return geo_union;
+    }
 
-	public String getUnit() {
-		return unit;
-	}
+    public void setGeo_union(String geo_union) {
+        this.geo_union = geo_union;
+    }
 
-	public void setUnit(String unit) {
-		this.unit = unit;
-	}
+    public String getUnit() {
+        return unit;
+    }
 
-	public String getReporting_year() {
-		return reporting_year;
-	}
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
 
-	public void setReporting_year(String reporting_year) {
-		this.reporting_year = reporting_year;
-	}
+    public String getReporting_year() {
+        return reporting_year;
+    }
 
-	public String getReporting_month() {
-		return reporting_month;
-	}
+    public void setReporting_year(String reporting_year) {
+        this.reporting_year = reporting_year;
+    }
 
-	public void setReporting_month(String reporting_month) {
-		this.reporting_month = reporting_month;
-	}
+    public String getReporting_month() {
+        return reporting_month;
+    }
 
-
+    public void setReporting_month(String reporting_month) {
+        this.reporting_month = reporting_month;
+    }
 
 }
